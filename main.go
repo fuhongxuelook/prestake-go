e06b0006ab334f2a71818338be7b2628c0926d6b
mengmengf <mengmengf@opera.com>
2022-03-20 16:34:00 +0800

    add cross origin

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	_ "prestake/routers"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
 
